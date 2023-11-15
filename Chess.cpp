def4b85f6974c49039fcdaae1da703d1786f6729
june <great.junekim@gmail.com>
2023-11-15 20:48:05 +0900

    remove GameLoop

--- Chess.cpp
+++ Chess.cpp
@@ -4,7 +4,6 @@
 #include "Chess.h"
 #include "colors.h"
 
-// #define A 97
 const int A = 97;
 const int D = 100;
 const int W = 119;
@@ -17,14 +16,6 @@ const int SPACE = 32;
 const int kBoardSize = 10;
 const int kFieldSize = 8;
 const int kBorderDist = 2;
-
-const char* pixelList[kBoardSize * kBoardSize];
-int bgColMap[kBoardSize * kBoardSize];
-int colorMap[kBoardSize * kBoardSize];
-
-
-void set(int x, int y, const char* var);
-
 
 #define null 0
 
@@ -44,11 +35,6 @@ int kRook = 4;
 int kQueen = 5;
 int kKing = 6;
 
-// set(1, 1, "♖"); 
-// set(1, 2, "♘"); set(1, 3, "♗"); set(1, 4, "♕"); set(1, 5, "♔"); set(1, 6, "♗"); set(1, 7, "♘"); set(1, 8, "♖");
-// set(2, 1, "♙"); set(2, 2, "♙"); set(2, 3, "♙"); set(2, 4, "♙"); set(2, 5, "♙"); set(2, 6, "♙"); set(2, 7, "♙"); set(2, 8, "♙");
-    // set(8, 1, "♜"); set(8, 2, "♞"); set(8, 3, "♝"); set(8, 4, "♛"); set(8, 5, "♚"); set(8, 6, "♝"); set(8, 7, "♞"); set(8, 8, "♜");
-    // set(7, 1, "♟"); set(7, 2, "♟"); set(7, 3, "♟"); set(7, 4, "♟"); set(7, 5, "♟"); set(7, 6, "♟"); set(7, 7, "♟"); set(7, 8, "♟");
 
 typedef struct Unit{
     int side; // 0(white) or 1(black)
@@ -129,196 +115,7 @@ void render() {
     }
 }
 
-int IsGame = 1;
 int delay = 16;
-struct Vector pointer;
-struct Vector origin;
-
-
-int canMove(struct Vector v, int dx, int dy)
-{
-    int x = dx + v.x;
-    int y = dy + v.y;
-
-    return x != 0 && y != 0 && x != kBoardSize - 1 && y != kBoardSize - 1;
-}
-
-int gameLoop(){
-    int underCol = kbBlack;
-    const char* underUni = "♖";
-    const char* catchUni = 0;
-    int pointerCol = kbGreen;
-    int catchCol = 0;
-    int underTCol = kfCyan;
-    int CatchUniTeam = 0;
-
-    do
-    {
-
-        int key = getKeyInput();
-        if (key == quit)
-            return 0;
-
-        otherSet(pointer.x, pointer.y, underTCol, colorMap);
-        otherSet(pointer.x, pointer.y, underCol, bgColMap);
-        set(pointer.x, pointer.y, underUni);
-
-        // key
-        if (key == A && canMove(pointer, -1, 0)){
-            pointer.x--;
-        } else if (key == D && canMove(pointer, 1, 0)){
-            pointer.x++;
-        } else if (key == S && canMove(pointer, 0, 1)){
-            pointer.y++;
-        } else if (key == W && canMove(pointer, 0, -1))
-            pointer.y--;
-        
-
-        underUni = pixelList[pointer.x + pointer.y * kBoardSize];
-        if (key == SPACE){
-            if (catchUni){
-                int King = (!strcmp(catchUni, "♔") || !strcmp(catchUni, "♚")); // all / 2 +28
-                int Queen = (!strcmp(catchUni, "♕") || !strcmp(catchUni, "♛"));
-                int Bishop = (!strcmp(catchUni, "♗") || !strcmp(catchUni, "♝"));
-                int Knight = (!strcmp(catchUni, "♘") || !strcmp(catchUni, "♞"));
-                int Rook = (!strcmp(catchUni, "♖") || !strcmp(catchUni, "♜"));
-                int Pawn = (!strcmp(catchUni, "♙") || !strcmp(catchUni, "♟"));
-
-                int dx = origin.x - pointer.x;
-                int dy = origin.y - pointer.y;
-                int adxbs = abs(dx);
-                int adybs = abs(dy);
-
-                int isReticle = (origin.x == pointer.x || origin.y == pointer.y);
-                int isDiagonal = (adxbs == adybs);
-
-                int moveDistance = (adxbs > adybs) * adxbs + (adxbs < adybs) * adybs + (adxbs == adybs) * adxbs;
-
-                float inclination_x = 0;
-                float inclination_y = 0;
-                
-                if (adxbs < adybs){
-                    if (dx != 0)
-                        inclination_x = (float)dx / (float)adybs;
-                    if (dy != 0)
-                        inclination_y = dy / adybs;
-                } else{
-                    if (dx != 0)
-                        inclination_x = dx / adxbs;
-                    if (dy != 0)
-                        inclination_y = (float)dy / (float)adxbs;
-                }
-
-                int i = 1;
-                for (;i<moveDistance; i++){
-                    int index = indexOf(pointer.x + (int)((float)i * inclination_x), pointer.y + (int)((float)i * inclination_y));
-                    // bgColMap[index] = kbGreen;
-                    if (strcmp(pixelList[index], " ")){
-                        break;
-                    }
-                }
-                
-                int cancollide = (i != moveDistance); //Knight와 같이 기울기가 1이하가 돼는 움직임은 충돌 검사를 두번.
-                
-                // if (!cancollide) {
-                //     for (;i<moveDistance; i++){
-                //         if (strcmp(pixelList[indexOf(pointer.x + (int)((float)i * inclination_x), pointer.y + (int)((float)i * inclination_y))], " ")){
-                            
-                //             break;
-                //         }
-                //     }               
-                // }
-
-                int pass = 0;
-                // printf("x, y : %f, %f, %d, %d, cancollide : %d, %d\n", inclination_x, inclination_y, dx, dy, cancollide, moveDistance);
-                // return 0;
-
-                if (King) {
-                    if ((isReticle || isDiagonal) && (adxbs == 1 || adybs == 1)) // 터치무브 규칙 쓰려면 (isReticle 과 isDiagonal 사이에) ^ 추가. 없에려면 || 추가
-                        pass = 1;
-                } else if (Queen) {
-                    if ((isReticle || isDiagonal) && !cancollide) // 터치무브 규칙 쓰려면 (isReticle 과 isDiagonal 사이에) ^ 추가. 없에려면 || 추가
-                        pass = 1;
-                } else if (Bishop) {
-                    if (isDiagonal && !cancollide) // 터치무브 규칙 쓰려면 && dx != 0 추가 또는 !isReticle. 규칙을 없에려면 isDiagonal만 
-                        pass = 1;
-                } else if (Knight) {
-                    if (!cancollide && ((adxbs == 2 && adybs == 1) || (adxbs == 1 && adybs == 2)))
-                        pass = 1;
-                } else if (Rook) {
-                    if (isReticle && !cancollide) // 터치무브 규칙 쓰려면 && dx != 0 추가 또는 && !isDiagonal. 규칙을 없에려면 isDiagonal만 
-                        pass = 1;
-                } else if (Pawn) {
-                    if (!cancollide && ((((CatchUniTeam * 2 -1) * ((origin.x == 2 || origin.x == 7) +1) == dx || CatchUniTeam * 2 -1 == dx) && adybs == (strcmp(underUni, " ")!=0)) || dx == 0 && dy == 0))
-                        pass = 1;
-                }
-
-                if (moveDistance == 0) // 전체 터치무브 규칙 적용 개별적용(X)
-                    pass = 1;
-
-                if (pass) {
-                    set(pointer.x, pointer.y, catchUni);
-                    otherSet(pointer.x, pointer.y, catchCol, colorMap);
-                    underUni = catchUni;
-                    catchUni = 0;
-                    pointerCol = kbGreen;
-                    catchCol = 0;
-                }
-            } else {
-                if (strcmp(pixelList[pointer.x + pointer.y * kBoardSize], " ")){
-                    catchUni = pixelList[pointer.x + pointer.y * kBoardSize];
-                    pointerCol = kbYellow;
-                    underUni = " ";
-                    catchCol = colorMap[pointer.x + pointer.y * kBoardSize];
-                    origin.x = pointer.x;
-                    origin.y = pointer.y;
-                    CatchUniTeam = 1;
-                    if (colorMap[pointer.x + pointer.y * kBoardSize] == kfCyan)
-                        CatchUniTeam = 0;
-                }
-            }
-        }
-        
-
-        underCol = bgColMap[pointer.x + pointer.y * kBoardSize];
-        underTCol = colorMap[pointer.x + pointer.y * kBoardSize];
-        otherSet(pointer.x, pointer.y, catchCol, colorMap);
-        otherSet(pointer.x, pointer.y, pointerCol, bgColMap);
-        if (catchUni){
-            set(pointer.x, pointer.y, catchUni);
-        }
-
-        // display
-        printf("%s\n", (pixelList[pointer.x + pointer.y * kBoardSize]));
-        printf("%d, %d\n" ,pointer.x, pointer.y);
-        printf(" \n");
-
-        draw();
-        printf("\n\n");
-        waitMs(delay);
-        system("clear");
-    } while (IsGame);
-    
-    return 0;
-}
-
-void draw(){
-    for (int i = 0; i < kBoardSize * kBoardSize; i++){
-        printf("\x1b[%d;%dm%s\x1b[0m", colorMap[i], bgColMap[i], pixelList[i]);
-        if ((i + 1) % kBoardSize == 0)
-            printf("\n");
-    }
-}
-
-void set(int x, int y, const char* var)
-{
-    pixelList[x + kBoardSize * y] = var;
-}
-
-void otherSet(int x, int y, int var, int *list)
-{
-    list[x + kBoardSize * y] = var;
-}
 
 int getKeyInput()
 {
@@ -333,15 +130,10 @@ void waitMs(int milliseconds)
     usleep(milliseconds * 1000);
 }
 
-int indexOf(int x, int y)
-{
-    return y * kBoardSize + x;
-}
 
 int main()
 {
     render();
 
     return 0;
-    return gameLoop();
 }
